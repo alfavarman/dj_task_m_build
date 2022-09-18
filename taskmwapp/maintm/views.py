785dca3081a311de3b535bc50e2732e4be1fd475
CKS <communicate2charles@gmail.com>
2022-09-18 22:41:21 +0700

    create pages for forms

--- taskmwapp/maintm/views.py
+++ taskmwapp/maintm/views.py
@@ -3,10 +3,20 @@ from django.http import HttpResponse
 from .models import ToDoList, Task
 # Create your views here.
 
-#
+
 def index(response, id):
 	ls = ToDoList.objects.get(id=id)
 	# value = Task.task.
 	return render(response, 'maintm/list.html', {"ls": ls})
 
 
+def home(response):
+	return render(response, 'maintm/home.html', {})
+
+
+def create(response):
+	return render(response, 'maintm/create.html', {})
+
+
+def about(response):
+	return render(response, 'maintm/about.html', {})
