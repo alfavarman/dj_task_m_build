72e2f68541533137973111d591282d7c232276eb
CKS <communicate2charles@gmail.com>
2022-09-18 16:20:01 +0700

    -admin created -added db to admin panel

--- taskmwapp/maintm/urls.py
+++ taskmwapp/maintm/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path("<int: id>", views.index), to set a variable in path
     path("", views.index),
     path("home/", views.index),
 ]
--- taskmwapp/maintm/views.py
+++ taskmwapp/maintm/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from .models import ToDoList, Task
 # Create your views here.
 
 
-def index(response):
-	return HttpResponse("<h1>Django Task Manager</h1>")
+def index(response): # add variable name into the function
+	#ls = ToDoList.objects.get(id=id) or task_list_name =
+	#task = ls.item_set.get(id=task ID number)
+	# return HttpResponse("<h1>Django Task Manager %s</h1><br>%s</br>" %(ls.task_list_name, str((item.task))
+	return HttpResponse("<h1>Django Task Manager</h1>")    # add %s inside %id outside <> #ls.task_list_name
 
 
 def home(response):
