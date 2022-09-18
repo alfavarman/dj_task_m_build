dfd0f70bcc97d29e58d10de3366fc3991ecebf22
CKS <communicate2charles@gmail.com>
2022-09-18 17:27:43 +0700

    django base template and variables

--- taskmwapp/maintm/views.py
+++ taskmwapp/maintm/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import ToDoList, Task
+from .models import ToDoList, Task
 # Create your views here.
 
 
-def index(response): # add variable name into the function
-	#ls = ToDoList.objects.get(id=id) or task_list_name =
-	#task = ls.item_set.get(id=task ID number)
-	# return HttpResponse("<h1>Django Task Manager %s</h1><br>%s</br>" %(ls.task_list_name, str((item.task))
-	return HttpResponse("<h1>Django Task Manager</h1>")    # add %s inside %id outside <> #ls.task_list_name
+def index(response, id):
+	ls = ToDoList.objects.get(id=id)
+	return render(response, 'maintm/base.html', {})
 
 
 def home(response):
-	return HttpResponse("<h1>Django Home Page!</h1>")
+	return render(response, 'maintm/home.html', {})
--- taskmwapp/taskmwapp/urls.py
+++ taskmwapp/taskmwapp/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('maintm.urls')),
-    path('index/', include('maintm.urls')),
+    path('', include('maintm.urls')),
 ]
