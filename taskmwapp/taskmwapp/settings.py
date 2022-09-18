661648a5dea7220d7841378b0c73b094f78be51b
CKS <communicate2charles@gmail.com>
2022-09-19 02:15:40 +0700

    regsettings crispy-forms

--- taskmwapp/taskmwapp/settings.py
+++ taskmwapp/taskmwapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'maintm.apps.MaintmConfig',
     'register.apps.RegisterConfig',
 ]
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRISPY_TEMPLATE_PACK="bootstrap4"
