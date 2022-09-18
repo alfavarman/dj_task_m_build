a106361a68655fa8846c747d0268f13c33197f66
CKS <communicate2charles@gmail.com>
2022-09-19 00:52:28 +0700

    sidebar menu added at base template

--- taskmwapp/maintm/urls.py
+++ taskmwapp/maintm/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("<int:id>", views.index, name='index'),
     path("create/", views.create, name='create'),
+    path("home/", views.home, name='home'),
+    path("about/", views.about, name='about'),
 ]
