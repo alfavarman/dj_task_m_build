4edc7a3f86da16d61562d969a38a7f7a30bace23
CKS <communicate2charles@gmail.com>
2022-09-18 15:37:59 +0700

    sqllite to postgress, add db models

--- taskmwapp/maintm/models.py
+++ taskmwapp/maintm/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDoList(models.Model):
+    tasks_list_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.tasks_list_name
+
+
+class Task(models.Model):
+    # on_delete=CASCADE if native key deleted forign follow
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    task = models.CharField(max_length=300)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.task
+
