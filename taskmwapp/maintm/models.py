d76a17761d12965aba112109dfee7c06ea017195
CKS <communicate2charles@gmail.com>
2022-09-19 14:44:41 +0700

    models modified with user, save boforedb reset

--- taskmwapp/maintm/models.py
+++ taskmwapp/maintm/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     tasks_list_name = models.CharField(max_length=150)
     objects = models.Manager()
 
